5f5e6451d1532078d485b42eb5dd1a86687efeaa
heinegyo <heinegyo@gmail.com>
2022-06-13 07:05:27 +0800

    project init

---
+++ gulimall-coupon/src/test/java/com/heingyo/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,14 @@
+package com.heingyo.gulimall.coupon;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
